c9637ee6550a921558764f2a30271f939134138f
Seungjin Kim <seungjin@duck.com>
2024-06-30 16:42:47 +0900

    20240508061350_insert_sample_account_row.sql is back

--- sparrow/migrations/20240508061350_insert_sample_account_row.sql
+++ sparrow/migrations/20240508061350_insert_sample_account_row.sql
@@ -2,7 +2,7 @@
 -- -- This pub/priv key are dev/test only. DO NOT USE IT FOR REAL.
 
 INSERT INTO account (
-    uuid,
+    uid,
     username,
     domain,
     private_key,
@@ -12,6 +12,7 @@ INSERT INTO account (
     note, 
     display_name,
     uri,
+    url,
     avatar_remote_url,
     header_remote_url,
     inbox_url,
@@ -24,13 +25,14 @@ INSERT INTO account (
     '018f56d7-bac8-7f76-a2eb-b089696db637',
     'seungjin',
     'dev.prefer.social',
-    replace('foo','\n',char(10)),
-    replace('bar','\n',char(10)),
+    replace('-----BEGIN PRIVATE KEY-----\nFOO\nBAR\n-----END PRIVATE KEY-----','\n',char(10)),
+    replace('-----BEGIN PUBLIC KEY-----\nFOO\nBAR\n-----END PUBLIC KEY-----','\n',char(10)),
     (unixepoch()),
     (unixepoch()),
     'note here',
     'seungjin_display_name',
     'https://dev.prefer.social',
+    'https://dev.prefer.social/@seungjin',
     'https://media-mstd.seungjin.net/accounts/avatars/109/737/937/659/013/254/original/626c9187e341632b.jpg',
     'https://media-mstd.seungjin.net/accounts/headers/109/737/937/659/013/254/original/9a714d77de20ae26.jpg',
     'https://dev.prefer.social/inbox',
@@ -40,3 +42,5 @@ INSERT INTO account (
     'https://dev.prefer.social/following',
     "Person"
 );
+
+
